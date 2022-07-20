0cf3f85279df7836973e11996c7e73ccee79a35f
Alvartinez <alvaro.martinezudes2@gmail.com>
2022-07-20 08:56:21 -0500

    Versión módulo 1

--- src/app/lineaModulo1/lineaModulo1.component.js
+++ src/app/lineaModulo1/lineaModulo1.component.js
@@ -7,3 +7,13 @@ function cierrelinea1(){
     document.getElementById("contendedorLinea1").classList.remove("ventanaabierta");
     document.getElementById("contendedorLinea1").classList.add("ventana");
 }
+
+function evalua(){
+    document.getElementById("evaluar").classList.remove("Evaluacion");
+    document.getElementById("evaluar").classList.add("evaluacionAbierta");
+}
+
+function cierreEvalua(){
+    document.getElementById("evaluar").classList.remove("evaluacionAbierta");
+    document.getElementById("evaluar").classList.add("Evaluacion");
+}
