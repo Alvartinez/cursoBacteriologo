eba0e83068be5d11eafa13cde8abcd8c1b043ff5
Alvartinez <alvaro.martinezudes2@gmail.com>
2022-07-26 01:17:23 -0500

    Versión modulo1

--- src/assets/javascript/block_Sesions.js
+++ src/assets/javascript/block_Sesions.js
@@ -1,10 +1,6 @@
 
-var ln1 = document.getElementById("line1");
-var ln2 = document.getElementById("line2");
-var ln3 = document.getElementById("line1");
-
 function animabarras(){
-    ln1.classList.add("activeline1");
-    ln2.classList.add("activeline2");
-    ln3.classList.add("activeline3");
+    document.getElementById("line1").classList.add("activeline1");
+    document.getElementById("line2").classList.add("activeline2");
+    document.getElementById("line3").classList.add("activeline3");
 }
