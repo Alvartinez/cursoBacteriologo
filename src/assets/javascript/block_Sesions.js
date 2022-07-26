e8d0d310fd7c78cc68cc1967f3c5f0f26d1213df
Alvartinez <alvaro.martinezudes2@gmail.com>
2022-07-26 02:03:17 -0500

    Avance m1

--- src/assets/javascript/block_Sesions.js
+++ src/assets/javascript/block_Sesions.js
@@ -1,6 +1,17 @@
 
 function animabarras(){
-    document.getElementById("line1").classList.add("activeline1");
-    document.getElementById("line2").classList.add("activeline2");
-    document.getElementById("line3").classList.add("activeline3");
+    var ln1 = document.getElementById("line1");
+    var ln2 = document.getElementById("line2");
+    var ln3 = document.getElementById("line3");
+    if(ln1.className == "activeline1"){
+        ln1.classList.remove("activeline1"); 
+        ln2.classList.remove("activeline2");
+        ln3.classList.remove("activeline3"); 
+        document.getElementById("pantalla").add("menu");
+    }else{
+        ln1.classList.add("activeline1"); 
+        ln2.classList.add("activeline2");
+        ln3.classList.add("activeline3");
+        document.getElementById("pantalla").remove("menu");
+    }
 }
