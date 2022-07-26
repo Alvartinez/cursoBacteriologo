32386ce3859e61ce388e33de3e2c7169761dd844
Alvartinez <alvaro.martinezudes2@gmail.com>
2022-07-26 01:10:13 -0500

    Update block_Sesions.js

--- src/assets/javascript/block_Sesions.js
+++ src/assets/javascript/block_Sesions.js
@@ -4,7 +4,7 @@ var ln2 = document.getElementById("line2");
 var ln3 = document.getElementById("line1");
 
 function animabarras(){
-    ln1.classList.toggle("activeline1");
-    ln2.classList.toggle("activeline2");
-    ln3.classList.toggle("activeline3");
+    ln1.classList.add("activeline1");
+    ln2.classList.add("activeline2");
+    ln3.classList.add("activeline3");
 }
