11015bbb7b0ce78d977083aa9f91725fa9186f46
Alvartinez <alvaro.martinezudes2@gmail.com>
2022-07-26 00:30:35 -0500

    Avance Modulo1

---
+++ src/assets/javascript/block_Sesions.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".contenedor").addEventListener("click", animabarras);
+
+var ln1 = document.querySelector(".line1");
+var ln2 = document.querySelector(".line2");
+var ln3 = document.querySelector(".line3");
